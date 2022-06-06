a10fd26f174b26df2d44a0676127a5f087023c1f
John <jmancoding@gmail.com>
2022-06-06 20:52:04 +1200

    Setup poetry

--- spec.md
+++
@@ -1,3 +0,0 @@
-## DEIMOS ENGINE 
-
-
--- src/setup.py
+++
@@ -1,9 +0,0 @@
-from setuptools import setup
-
-setup(
-    name='deimos',
-    version='0.0.1',
-    description='A engine for small demos',
-    py_modules=['deimos'],
-    package_dir={'': 'src'}
-)
