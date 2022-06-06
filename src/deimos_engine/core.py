77d9eca447bd2e16eb8fdf0baf2ebd33e35d7aad
John <jmancoding@gmail.com>
2022-06-06 21:43:56 +1200

    setup tests

---
+++ src/deimos_engine/core.py
@@ -0,0 +1,12 @@
+import pygame as pg
+
+def initialize():
+    pg.init()
+
+def demo():
+    window = pg.display.set_mode((500, 500))
+    while True:
+        pass
+
+def main():
+    print("success")
